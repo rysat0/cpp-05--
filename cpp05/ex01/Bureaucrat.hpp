acbc4c3619af2a4dafbf13c0a2b9acd0088ab913
rysat0 <leonarudoryuu0629@gmail.com>
2026-09-24 17:44:09 +0900

    add: cpp05/ex01 halfway to Form.cpp complete

--- cpp05/ex01/Bureaucrat.hpp
+++ cpp05/ex01/Bureaucrat.hpp
@@ -26,6 +26,8 @@ class Bureaucrat
 		void incrementGrade();
 		void decrementGrade();
 
+		void signForm(Form& form) const;
+
 		class GradeTooHighException : public std::exception
 		{
 			public:
--- cpp05/ex01/Form.cpp
+++ cpp05/ex01/Form.cpp
@@ -1,2 +1,42 @@
 #include "Form.hpp"
 #include "Bureaucrat.hpp"
+
+
+const char* Form::GradeTooHighException::what() const throw()
+{
+	return("Grade is too high.");
+}
+
+const char* Form::GradeTooLowException::what() const throw()
+{
+	return("Grade is too low.");
+}
+
+Form::Form() : _name("default form"), _isSigned(false), _gradeToSign(75), _gradeToExecute(50)
+{
+}
+
+Form::Form(const std::string& name, int gradeToSign, int gradeToExecute) : _name(name), _isSigned(false), _gradeToSign(gradeToSign), _gradeToExecute(gradeToExecute)
+{
+	if(this->_gradeToSign < 1 || this->_gradeToExecute < 1)
+		throw GradeTooHighException();
+	else if(this->_gradeToSign > 150 || this->_gradeToExecute > 150)
+		throw GradeTooLowException();
+}
+
+Form::Form(const Form& other) : _name(other._name), _isSigned(other._isSigned), _gradeToSign(other._gradeToSign), _gradeToExecute(other._gradeToExecute)
+{
+}
+
+Form& Form::operator=(const Form& other)
+{
+	if(this != &other)
+		this->_isSigned = other._isSigned;
+	return(*this);
+}
+
+Form::~Form()
+{
+}
+
+
